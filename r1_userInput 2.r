84de8a94d96be0b0f3a19c01909092a2dc5dcce5
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2024-11-25 03:48:27 +0800

    R User Input 2.

---
+++ r1_userInput 2.r
@@ -0,0 +1,13 @@
+print("=====================")
+print("| Learning R #1.2  |")
+print("|     User-Input   |")
+print("=====================")
+print("Hello world!")
+
+name <- readline("Please, enter your name: ")
+age <- readline("Enter your age: ")
+fave_food <- readline("What's your favorite food: ")
+
+print(paste("Hi, ", name, "!"))
+print(paste("You're ", age, " years old!"))
+print(paste("Your favorite food is '", fave_food, "'!"))
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
+Hi there!
+
 Welcome! This is my "R" programming language repository. This is just me practicing R and uploading my assignments in Github.
+
+This will be a journey of my Data Science journey!
