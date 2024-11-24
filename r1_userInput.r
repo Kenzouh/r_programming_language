e1671907c054011f30f20535058c0563ba8095b5
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2024-11-25 03:40:19 +0800

    My R Projects.

---
+++ r1_userInput.r
@@ -0,0 +1,9 @@
+print("=====================")
+print("| Learning R #1  |")
+print("|    User-Input |")
+print("=====================")
+print("Hello world!")
+
+name <- readline("Please, enter your name: ")
+# paste() = Allows you to concatenate elements.
+print(paste("Hi, ", name, "!"))
--- r_assignments/readme.md
+++
@@ -1,1 +0,0 @@
-Coding my assignments in R.
