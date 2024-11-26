bfacefd3c78194be597d890f69edc2da1b94a5b8
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2024-11-27 00:53:56 +0800

    R Variables 2.

---
+++ r2_variables2.r
@@ -0,0 +1,12 @@
+print("======================")
+print("|  Learning R #2.2   |")
+print("|    Variables 2     |")
+print("======================")
+
+adjective <- "amazing"
+print(paste("You are ", adjective, "! :)"))
+
+# adj = adjective
+adj1 <- adj2 <- adj3 <- "Cute"
+
+print(paste("adj1 = ", adj1))
